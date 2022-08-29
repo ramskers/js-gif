715b02b3ff7cfefbb770493533c176a66f169387
Your Name <you@example.com>
2022-08-29 15:36:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useSpring, animated } from "react-spring";
+import "./App.css";
 
 function App() {
+  const [num, setNum] = useState(0);
+  const props = useSpring({ val: num, from: { val: 0 } });
+
+  const handleClick = () => {
+    setNum(rand(1, 10000));
+  };
+
+  function rand(min, max) {
+    let randomNum = Math.random() * (max - min) + min;
+    if (1000 >= randomNum) {
+      setTimeout(function () {
+        alert("The site says... You have won!");
+      }, 1000);
+    } else {
+      setTimeout(function () {
+        alert("The site says... Too bad, you have lost!");
+      }, 1000);
+    }
+    return Math.round(randomNum);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>The JavaScript Lottery</h1>
+      <h3>Click the button the draw a number.</h3>
+      <div>
+        <button onClick={handleClick}>Lets Play!</button>
+      </div>
+      <p>
+        Your Number:
+        <animated.div className="number">
+          {props.val.interpolate((val) => Math.round(val))}
+        </animated.div>
+      </p>
+      <p>* Any number in the 1 to 1000 range wins!</p>
     </div>
   );
 }
